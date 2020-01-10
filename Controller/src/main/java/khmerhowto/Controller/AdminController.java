4947c2b84864f47cf61c97a08321b162357749ba
sok menghok <soksomeungkhun@gmail.com>
2020-01-10 14:24:58 +0700

    add customize admin info, link menu , add category

--- Controller/src/main/java/khmerhowto/Controller/AdminController.java
+++ Controller/src/main/java/khmerhowto/Controller/AdminController.java
@@ -18,4 +18,9 @@ public class AdminController {
     String category(){
         return "admin/admin-category";
     }
+
+    @GetMapping("/admin/customize")
+    String customizeInfo(){
+        return "admin/admin-customize-user";
+    }
 }
