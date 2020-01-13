fcf24f33a2d5b28613a95aef661c3c33896ff198
madame nich <huot.nich8888@gmail.com>
2020-01-13 14:12:53 +0700

    working on manage user page

--- Controller/src/main/java/khmerhowto/Controller/AdminController.java
+++ Controller/src/main/java/khmerhowto/Controller/AdminController.java
@@ -1,13 +1,25 @@
 package khmerhowto.Controller;
 
+import khmerhowto.Repository.Model.User;
+import khmerhowto.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 /**
  * AdminController
  */
 @Controller
 public class AdminController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/admin/feedback")
     String manageFeedBack(){
         return "admin/admin-feedback";
@@ -17,7 +29,13 @@ public class AdminController {
         return "admin/admin-question";
     }
     @GetMapping("/admin/user")
-    String manageUser(){
+    String manageUser(@ModelAttribute("User")User user, @PageableDefault(size = 10)Pageable pageable, Model model){
+        Page<User> page =userService.findAll(pageable);
+        List<User> users = page.getContent();
+        model.addAttribute("page",page);
+        model.addAttribute("users",users);
+
+
         return "admin/admin-user";
     }
     @GetMapping("/admin")
