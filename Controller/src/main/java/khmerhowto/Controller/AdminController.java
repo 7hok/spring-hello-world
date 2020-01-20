2dcb9c725a690417b6df9a586dcf548b21af26d1
pengsea <pengsea.long@gmail.com>
2020-01-20 15:07:25 +0700

    solve conflict on con

--- Controller/src/main/java/khmerhowto/Controller/AdminController.java
+++ Controller/src/main/java/khmerhowto/Controller/AdminController.java
@@ -143,8 +143,5 @@ public class AdminController {
         return "admin/admin-article";
     }
 
-    @GetMapping("/admin/article/insert")
-    String insertArticle(){
-        return "admin/testartcle";
-    }
+
 }
