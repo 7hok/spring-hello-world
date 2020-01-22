422fc756107413baf690f3945b74d6f53f115232
pengsea <pengsea.long@gmail.com>
2020-01-22 15:40:56 +0700

    comment and read content to show

--- Repository/src/main/java/khmerhowto/Repository/CommentRepository.java
+++ Repository/src/main/java/khmerhowto/Repository/CommentRepository.java
@@ -1,10 +1,21 @@
 package khmerhowto.Repository;
 
 import khmerhowto.Repository.Model.Comment;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+ 
 
 @RepositoryRestResource
-public interface CommentRepository extends JpaRepository<Comment,Integer> {
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    
+   public Page<Comment> findAllBycontentIdOrderByTimestamp( Integer id,Pageable pageable);
 
+   @Query(value = "SELECT Count(c) FROM Comment c")
+   public Integer getTotalComment();
 }
---
+++ Service/src/main/java/khmerhowto/Service/ServiceImplement/CommentServiceImp.java
@@ -0,0 +1,20 @@
+package khmerhowto.Service.ServiceImplement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import khmerhowto.Repository.CommentRepository;
+import khmerhowto.Service.CommentService;
+
+@Service
+public class CommentServiceImp implements CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    public Integer getTotalComment() {
+        return commentRepository.getTotalComment();
+    }
+     
+
+}
