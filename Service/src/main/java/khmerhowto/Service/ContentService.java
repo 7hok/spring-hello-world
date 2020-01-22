f7787ef7a847bb5ab63414736e5911ce5fb90c85
humchamroeun <humchamroeuncs@gmail.com>
2020-01-22 16:29:52 +0700

    solve conflict

--- Service/src/main/java/khmerhowto/Service/ContentService.java
+++ Service/src/main/java/khmerhowto/Service/ContentService.java
@@ -1,4 +1,11 @@
 package khmerhowto.Service;
 
+
+
+import khmerhowto.Repository.Model.Content;
+
+import java.util.List;
+
 public interface ContentService {
+    List <Content> findAll();
 }
--- Service/src/main/java/khmerhowto/Service/ServiceImplement/ContentServiceImpl.java
+++ Service/src/main/java/khmerhowto/Service/ServiceImplement/ContentServiceImpl.java
@@ -1,4 +1,21 @@
 package khmerhowto.Service.ServiceImplement;
 
-public class ContentServiceImpl {
+
+import khmerhowto.Repository.ContentRepository;
+import khmerhowto.Repository.Model.Content;
+import khmerhowto.Service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContentServiceImpl implements ContentService {
+    @Autowired
+    private ContentRepository contentRepository;
+
+    @Override
+    public List<Content> findAll() {
+        return contentRepository.findAll();
+    }
 }
