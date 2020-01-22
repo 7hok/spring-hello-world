83c0aae705faf31bc353466d12e31c5aee29c4d3
pengsea <pengsea.long@gmail.com>
2020-01-22 16:21:38 +0700

    like

---
+++ Service/src/main/java/khmerhowto/Service/ServiceImplement/InterestedServiceImp.java
@@ -0,0 +1,29 @@
+package khmerhowto.Service.ServiceImplement;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import khmerhowto.Repository.FeedbackRepository;
+import khmerhowto.Repository.InterestRepository;
+import khmerhowto.Repository.Model.Feedback;
+import khmerhowto.Service.InterestedService;
+
+/**
+ * FeedBackServiceImp
+ */
+@Service
+public class InterestedServiceImp implements InterestedService{
+    
+    @Autowired
+    private InterestRepository interestRepository;
+
+    @Override
+    public Integer getTotalLike() {
+
+        return interestRepository.getTotalLike();
+    }
+}
