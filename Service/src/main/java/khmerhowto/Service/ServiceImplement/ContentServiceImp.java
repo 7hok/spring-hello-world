738febf8af389e8101b3f446c9b3563009aed328
pengsea <pengsea.long@gmail.com>
2020-01-28 08:34:49 +0700

    conflict

--- Service/src/main/java/khmerhowto/Service/ServiceImplement/ContentServiceImp.java
+++ Service/src/main/java/khmerhowto/Service/ServiceImplement/ContentServiceImp.java
@@ -1,5 +1,7 @@
 package khmerhowto.Service.ServiceImplement;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,20 +9,27 @@ import org.springframework.stereotype.Service;
 
 import khmerhowto.Repository.ContentRepository;
 import khmerhowto.Repository.Model.Content;
+import khmerhowto.Service.ContentService;
 
 /**
  * ContentServiceImp
  */
 @Service
-public class ContentServiceImp {
+public class ContentServiceImp implements ContentService {
     @Autowired
     private ContentRepository contentRepository;
 
-	public Page<Content> findAll(Pageable pageable) {
-		return contentRepository.findAll(pageable);
-	}
-
-	public Page<Content> findByName(String string,Pageable pageable) {
-        return contentRepository.findByTitleContainingIgnoreCase(string,pageable);
+    public Page<Content> findAll(Pageable pageable) {
+        return contentRepository.findAll(pageable);
     }
+
+    public Page<Content> findByName(String string, Pageable pageable) {
+        return contentRepository.findByTitleContainingIgnoreCase(string, pageable);
+    }
+
+    @Override
+    public Page<Content> findAllByOrderByIdDesc(Pageable pageable) {
+        return contentRepository.findAllByOrderByIdDesc(pageable);
+    }
+ 
 }
