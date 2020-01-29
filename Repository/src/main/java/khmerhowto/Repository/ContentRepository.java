816fc5b7fce4a7b4345e82cd99d888f57b1658cf
humchamroeun <humchamroeuncs@gmail.com>
2020-01-29 08:24:23 +0700

    merge conflict

--- Repository/src/main/java/khmerhowto/Repository/ContentRepository.java
+++ Repository/src/main/java/khmerhowto/Repository/ContentRepository.java
@@ -1,5 +1,6 @@
 package khmerhowto.Repository;
 
+import khmerhowto.Repository.Model.Category;
 import khmerhowto.Repository.Model.Content;
 
 import org.springframework.data.domain.Page;
@@ -8,8 +9,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
-public interface ContentRepository extends JpaRepository<Content,Integer> {
+public interface ContentRepository extends JpaRepository<Content, Integer> {
 
 	Page<Content> findByTitleContainingIgnoreCase(@Param("title") String title, Pageable pageable);
+
+	Category findContentsByCategoryId(Integer id);
+	List<Content> findFirst2ByCategoryId(Integer id);
+
 }
