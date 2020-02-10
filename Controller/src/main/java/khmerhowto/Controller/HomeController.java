43e31a1027da14a8630e66aca603381c3923901f
humchamroeun <humchamroeuncs@gmail.com>
2020-02-10 09:03:26 +0700

    merge

--- Controller/src/main/java/khmerhowto/Controller/HomeController.java
+++ Controller/src/main/java/khmerhowto/Controller/HomeController.java
@@ -22,6 +22,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -385,10 +387,25 @@ public class HomeController {
     }
     @GetMapping("/category/s/{id}")
     String categoryLeft(Model model, @PathVariable Integer id){
-\        model.addAttribute("categories",categoryServiceImpl.findAll());
+        model.addAttribute("categories",categoryServiceImpl.findAll());
         model.addAttribute("OneCategory",categoryRepository.findByIdAndStatus(id,1));
 
         return "content-by-category-test";
+    }
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    @GetMapping("/sendEmailHz")
+    @ResponseBody
+    public  void sendEmail() {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        String[] emailArray= {"humchamroeunhrd@gmail.com", "humchamroeuncs@gmail.com", "humchamroeunmmo@gmail.com"};
+        msg.setTo(emailArray);
+        msg.setSubject("Kunloes Update");
+        msg.setText("Find a beautiful piece of art. If you fall in love with Van Gogh or Matisse or John Oliver Killens, or if you fall love with the music of Coltrane, the music of Aretha Franklin, or the music of Chopin - find some beautiful art and admire it, and realize that that was created by human beings just like you, no more human, no less. Read more at https://www.brainyquote.com/topics/beautiful-quotes");
+        javaMailSender.send(msg);
+        System.out.println("send email");
     }
   
 }
