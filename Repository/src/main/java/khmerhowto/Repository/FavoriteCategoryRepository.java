d19ecaa72d4db54711f26d734baee77ce08c58be
humchamroeun <humchamroeuncs@gmail.com>
2020-02-12 13:54:59 +0700

    merge

--- Repository/src/main/java/khmerhowto/Repository/FavoriteCategoryRepository.java
+++ Repository/src/main/java/khmerhowto/Repository/FavoriteCategoryRepository.java
@@ -4,7 +4,9 @@ import khmerhowto.Repository.Model.FavoriteCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface FavoriteCategoryRepository extends JpaRepository<FavoriteCategory,Integer> {
-
+    List<FavoriteCategory> findAllByuserId(Integer id);
 }
