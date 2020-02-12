c3cb37e3028b88ced6ac221e78d75a1daaaa4540
humchamroeun <humchamroeuncs@gmail.com>
2020-02-12 09:09:15 +0700

    merge controller

--- Controller/src/main/java/khmerhowto/Controller/HomeController.java
+++ Controller/src/main/java/khmerhowto/Controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 
 import com.corundumstudio.socketio.SocketIOServer;
@@ -413,16 +414,21 @@ public class HomeController {
     @Autowired
     JavaMailSender javaMailSender;
 
+
     @GetMapping("/sendEmailHz")
     @ResponseBody
     public  void sendEmail() {
+
         SimpleMailMessage msg = new SimpleMailMessage();
-        String[] emailArray= {"humchamroeunhrd@gmail.com", "humchamroeuncs@gmail.com", "humchamroeunmmo@gmail.com"};
-        msg.setTo(emailArray);
-        msg.setSubject("Kunloes Update");
-        msg.setText("Find a beautiful piece of art. If you fall in love with Van Gogh or Matisse or John Oliver Killens, or if you fall love with the music of Coltrane, the music of Aretha Franklin, or the music of Chopin - find some beautiful art and admire it, and realize that that was created by human beings just like you, no more human, no less. Read more at https://www.brainyquote.com/topics/beautiful-quotes");
-        javaMailSender.send(msg);
-        System.out.println("send email");
+        for (int i=0;i<=5;i++){
+            String[] emailArray= {"humchamroeunhrd@gmail.com", "humchamroeuncs@gmail.com", "humchamroeunmmo@gmail.com"};
+            msg.setTo(emailArray);
+            msg.setSubject("Kunloes Update");
+            msg.setText("Find a beautiful piece of art. If you fall in love with Van Gogh or Matisse or John Oliver Killens, or if you fall love with the music of Coltrane, the music of Aretha Franklin, or the music of Chopin - find some beautiful art and admire it, and realize that that was created by human beings just like you, no more human, no less. Read more at https://www.brainyquote.com/topics/beautiful-quotes");
+            javaMailSender.send(msg);
+            System.out.println("send email");
+            System.out.println("Jab id"+favoriteCategoryRepository.findAllByuserId(1));
+        }
     }
   
 }
